5dda1ce3e3d9d749c5919e4da1956a8927632f2b
Chandrasekaran <chandrasekaran@tartlabs.com>
2019-01-02 20:02:11 +0530

    Logging Out

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 import '../setupTests';
+
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
-    expect(wrapper).toMatchSnapshot();
-    
-    // expect(toJson(wrapper)).toMatchSnapshot();
-    // expect(wrapper.find('h1').text()).toBe('Expensify');
-    // const renderer = new ReactShallowRenderer();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header startLogout={() => { }} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
